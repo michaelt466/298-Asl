abd5017810f8fe2c57ff6d921051663ef12b1b62
matayl26 <matayl26@ncsu.edu>
2026-04-23 10:51:57 -0400

    extracted duplicate stuff into a function

--- live_demo.py
+++ live_demo.py
@@ -1,23 +1,17 @@
-
-import cv2				# Import OpenCV for image processing
-import sys				# Import for time
-import os		# Import for reading files
-import threading		# Import for separate thread for image classification
-import numpy as np 		# Import for converting vectors
+import cv2
+import sys
+import os
+import threading
+import numpy as np
 import display
 import spell_checker as spell_checker
 import voice as voice
-#import spell_checker	# Import for spelling corrections
 
 # Disable tensorflow compilation warnings
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2' 
-#*** import tensorflow as tf # Import tensorflow for Inception Net's backend
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# added this new import
 import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
-
-
 
 # Language in which you want to convert
 language = 'en'
@@ -33,179 +27,115 @@ label_lines = [line.rstrip() for line in tf.gfile.GFile("training_set_labels.txt
 
 # Load trained model's graph
 with tf.gfile.FastGFile("trained_model_graph.pb", 'rb') as f:
-	# Define a tensorflow graph
     graph_def = tf.GraphDef()
-
-    # Read and import line by line from the trained model's graph
     graph_def.ParseFromString(f.read())
     _ = tf.import_graph_def(graph_def, name='')
 
-def predict(image_data):
 
-	# Focus on Region of Interest (Image within the bounding box)
-	resized_image = image_data[70:350, 70:350]
-	
-	# Resize to 200 x 200
-	resized_image = cv2.resize(resized_image, (200, 200))
+def predict(sess, softmax_tensor, image_data):
+    # Focus on Region of Interest (Image within the bounding box)
+    resized_image = image_data[70:350, 70:350]
 
-	#changed this line to tobytes() from tostring()
-	image_data = cv2.imencode('.jpg', resized_image)[1].tobytes()
+    # Resize to 200 x 200
+    resized_image = cv2.resize(resized_image, (200, 200))
 
-	predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
+    image_data = cv2.imencode('.jpg', resized_image)[1].tobytes()
 
-	# Sort to show labels of first prediction in order of confidence
-	top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+    predictions = sess.run(softmax_tensor, {'DecodeJpeg/contents:0': image_data})
 
-	max_score = 0.0
-	res = ''
-	for node_id in top_k:
-		# Just to get rid of the Z error for demo
-		if label_lines[node_id].upper() == 'Z':
-			human_string = label_lines[node_id+1]
-		else:
-			human_string = label_lines[node_id]
-		score = predictions[0][node_id]
-		if score > max_score:	
-			max_score = score
-			res = human_string
+    # Sort to show labels of first prediction in order of confidence
+    top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
 
-	return res, max_score
+    max_score = 0.0
+    res = ''
+    for node_id in top_k:
+        # Just to get rid of the Z error for demo
+        if label_lines[node_id].upper() == 'Z':
+            human_string = label_lines[node_id + 1]
+        else:
+            human_string = label_lines[node_id]
+        score = predictions[0][node_id]
+        if score > max_score:
+            max_score = score
+            res = human_string
+
+    return res, max_score
+
 
 def speak_letter(letter):
-    # Create the text to be spoken
-    prediction_text = letter
-    voice.speak(prediction_text)
-    
+    voice.speak(letter)
+
+
+def process_letter(sess, softmax_tensor, img):
+    """Run prediction on a frame and update current_word. Returns updated word."""
+    global current_word
+
+    letter, score = predict(sess, softmax_tensor, img)
+    letter_upper = letter.upper()
+
+    print("Letter:", letter_upper, " Score:", score)
+    print("Current word:", current_word)
+
+    if letter_upper not in ('NOTHING', 'SPACE', 'DEL'):
+        current_word += letter_upper
+        speak_letter(letter)
+
+    elif letter_upper in ('SPACE', 'DEL'):
+        if len(current_word) > 0:
+            speak_letter(current_word)
+        current_word = ""
+
+    elif letter_upper == 'NOTHING':
+        pass
+
+    else:
+        print("UNEXPECTED INPUT:", letter_upper)
+
+
+# Flags and counters — defined before the loop
+time_counter = 0
+captureFlag = False
+realTime = True
+spell_check = False
+
+window_name = display.setup_display("Live Stream")
 
 with tf.Session() as sess:
-	# Feed the image_data as input to the graph and get first prediction
-	softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+    softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
 
-	# Global variable to keep track of time
-	time_counter = 0
+    while True:
+        keypress = cv2.waitKey(1)
 
-	# Flag to check if 'c' is pressed
-	captureFlag = False
+        img = live_stream.read()[1]
 
-	# Toggle real time processing
-	realTime = True
+        height, width = img.shape[:2]
 
-	# Toggle spell checking
-	spell_check = False
+        display.update_display(img, current_word, window_name)
 
-	# Setup the live display window
-	window_name = display.setup_display("Live Stream")
-	LOOP = False; # just a label for me to find the start of the loop
-	while True:
+        # Real-time mode: predict every 45 frames
+        if time_counter % 45 == 0 and realTime:
+            process_letter(sess, softmax_tensor, img)
 
-		# Display live feed until ESC key is pressed
-		# Press ESC to exit
-		keypress = cv2.waitKey(1)
+        # 'C' pressed — switch to capture mode
+        if keypress == ord('c'):
+            captureFlag = True
+            realTime = False
 
-		# Flip the image laterally
-		#img = cv2.flip(img, 1)
-		
-		# TESTING:
-		#threading.Timer(5.0, printit).start()
-		
-		# Read a single frame from the live feed
-		img = live_stream.read()[1]
+        # 'R' pressed — resume real-time mode
+        if keypress == ord('r'):
+            realTime = True
 
+        # Capture mode: predict on demand
+        if captureFlag:
+            captureFlag = False
+            process_letter(sess, softmax_tensor, img)
 
-		# Get frame dimensions *** MIGHT SLOW A LOT
-		#ret, frame = live_stream.read()
-		height, width = img.shape[:2]
+        # ESC pressed — exit
+        if keypress == 27:
+            break
 
-		# Used to setup display
-		display.update_display(img, current_word, window_name)
+        time_counter += 1
 
-		# To get time intervals
-		if time_counter % 45 == 0 and realTime:
-
-			letter, score = predict(img)
-			
-			
-			#cv2.imshow("Resized Image", img)
-			print("Letter: ",letter.upper(), " Score: ", score)
-			print("Current word: ", current_word)
-
-			if letter.upper() != 'NOTHING' and letter.upper() != 'SPACE' and letter.upper() != 'DEL':
-				current_word += letter.upper()
-				speak_letter(letter)
-
-			# Say the letter out loud ***USED TO SAY SPACE
-			if letter.upper() == 'SPACE' or letter.upper() == 'DEL':
-				if len(current_word) > 0:
-					if True:
-						speak_letter(current_word)#spell_checker.correction(current_word)
-					else:
-						speak_letter(current_word)
-				current_word = ""
-
-			
-			elif letter.upper() == 'NOTHING':
-				pass
-
-			else:
-				print("UNEXPECTED INPUT: ", letter.upper())
-
-
-		# 'C' is pressed
-		if keypress == ord('c'):
-			captureFlag = True
-			realTime = False
-
-		# 'R' is pressed
-		if keypress == ord('r'):
-			realTime = True
-
-		if captureFlag:
-			captureFlag = False
-
-			# Show the image considered for classification
-			# Just for Debugging
-			#cv2.imshow("Resized Image", resized_image)
-
-			# Get the letter and the score
-			letter, score = predict(img)
-
-			#take gui stuff here if we want ***
-			print("Letter: ",letter.upper(), " Score: ", score)
-			print("Current word: ", current_word)
-
-
-			if letter.upper() != 'NOTHING' and letter.upper() != 'SPACE' and letter.upper() != 'DEL':
-				current_word += letter.upper()
-				speak_letter(letter)
-
-			# Say the letter out loud ***USED TO SAY SPACE
-			if letter.upper() == 'SPACE' or letter.upper() == 'DEL':
-				if len(current_word) > 0:
-					if True:
-						speak_letter(current_word)#spell_checker.correction(current_word)
-					else:
-						speak_letter(current_word)
-				current_word = ""
-
-			#elif letter.upper() == 'DEL':
-				#if len(current_word) > 0:
-					#current_word = current_word[:-1]
-			
-			elif letter.upper() == 'NOTHING':
-				pass
-
-			else:
-				print("UNEXPECTED INPUT: ", letter.upper())
-
-		# If ESC is pressed
-		if keypress == 27:
-			exit(0)	
-
-		# Update time
-		time_counter = time_counter + 1
-
-# Stop using camers
+# Release resources
 live_stream.release()
-
-# Destroy windows created by OpenCV
 cv2.destroyAllWindows()
